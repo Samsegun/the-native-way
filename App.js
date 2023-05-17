ba539494468076ebe977c6e889823c88e0be65ca
Samsegun <oyebadesamsegun@gmail.com>
2023-05-17 20:45:19 +0100

    list exercise

--- App.js
+++ App.js
@@ -2,31 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 import Texter from './components/Texter';
 import ColorBox from './components/ColorBox';
+import ListsExercise from './components/ListsExercise';
 
 const App = () => {
-  // const viewData = [
-  //   {
-  //     viewBg: { backgroundColor: '#2aa198' },
-  //     textStyle: { color: '#fff' },
-  //     text: 'Cyan #2aa198',
-  //   },
-  //   {
-  //     viewBg: { backgroundColor: '#268bd2' },
-  //     textStyle: { color: '#fff' },
-  //     text: 'Blue #268bd2',
-  //   },
-  //   {
-  //     viewBg: { backgroundColor: '#d33682' },
-  //     textStyle: { color: '#fff' },
-  //     text: 'Magenta #d33682',
-  //   },
-  //   {
-  //     viewBg: { backgroundColor: '#cb4b16' },
-  //     textStyle: { color: '#fff' },
-  //     text: 'Orange #cb4b16',
-  //   },
-  // ];
-
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
@@ -34,7 +12,8 @@ const App = () => {
           Here are some boxes of different colours
         </Text>
 
-        <ColorBox />
+        {/* <ColorBox /> */}
+        <ListsExercise />
       </View>
     </SafeAreaView>
   );
@@ -60,19 +39,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 5,
   },
-  // box1: {
-  //   color: '#fff',
-  //   backgroundColor: '#2aa198',
-  // },
-  // box2: {
-  //   backgroundColor: '#268bd2',
-  // },
-  // box3: {
-  //   backgroundColor: '#d33682',
-  // },
-  // box4: {
-  //   backgroundColor: '#cb4b16',
-  // },
 });
 
 export default App;
--- components/ColorBox.js
+++ components/ColorBox.js
@@ -1,27 +1,55 @@
 import React from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, SectionList } from 'react-native';
 
 const DATA = [
   {
-    viewBg: { backgroundColor: '#2aa198' },
-    textStyle: { color: '#fff' },
-    text: 'Cyan #2aa198',
+    title: 'primary colors',
+    data: [
+      {
+        viewBg: { backgroundColor: '#2aa198' },
+        textStyle: { color: '#fff' },
+        text: 'Cyan #2aa198',
+      },
+      {
+        viewBg: { backgroundColor: '#268bd2' },
+        textStyle: { color: '#fff' },
+        text: 'Blue #268bd2',
+      },
+    ],
+    // viewBg: { backgroundColor: '#2aa198' },
+    // textStyle: { color: '#fff' },
+    // text: 'Cyan #2aa198',
   },
+  //   {
+  //     title: 'primary colors',
+  //     viewBg: { backgroundColor: '#268bd2' },
+  //     textStyle: { color: '#fff' },
+  //     text: 'Blue #268bd2',
+  //   },
   {
-    viewBg: { backgroundColor: '#268bd2' },
-    textStyle: { color: '#fff' },
-    text: 'Blue #268bd2',
+    title: 'secondary colors',
+    data: [
+      {
+        viewBg: { backgroundColor: '#d33682' },
+        textStyle: { color: '#fff' },
+        text: 'Magenta #d33682',
+      },
+      {
+        viewBg: { backgroundColor: '#cb4b16' },
+        textStyle: { color: '#fff' },
+        text: 'Orange #cb4b16',
+      },
+    ],
+    // viewBg: { backgroundColor: '#d33682' },
+    // textStyle: { color: '#fff' },
+    // text: 'Magenta #d33682',
   },
-  {
-    viewBg: { backgroundColor: '#d33682' },
-    textStyle: { color: '#fff' },
-    text: 'Magenta #d33682',
-  },
-  {
-    viewBg: { backgroundColor: '#cb4b16' },
-    textStyle: { color: '#fff' },
-    text: 'Orange #cb4b16',
-  },
+  //   {
+  // title: 'primary colors',
+  // viewBg: { backgroundColor: '#cb4b16' },
+  // textStyle: { color: '#fff' },
+  // text: 'Orange #cb4b16',
+  //   },
 ];
 
 const Item = ({ viewBg, viewText, textStyle }) => {
@@ -45,10 +73,19 @@ const ColorBox = () => {
 
   return (
     <>
-      <FlatList
+      {/* <FlatList
         data={DATA}
         renderItem={renderItem}
         keyExtractor={(item) => item.text}
+      /> */}
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item) => item.text}
+        renderItem={renderItem}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.head}>{title}</Text>
+        )}
+        ListEmptyComponent={<Text>list is empty</Text>}
       />
     </>
   );
@@ -60,6 +97,10 @@ const styles = StyleSheet.create({
     marginVertical: 4,
     alignItems: 'center',
     borderRadius: 5,
+  },
+  head: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
